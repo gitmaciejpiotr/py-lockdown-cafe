54d7554e71936d2f0a069ccfee8c375a889e6cbd
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-02-02 12:33:09 +0100

    fixed logic bug

--- app/main.py
+++ app/main.py
@@ -11,6 +11,8 @@ def go_to_cafe(friends: list[dict], cafe: Cafe) -> str | None:
         except VaccineError:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
-            return f"Friends should buy {masks_to_buy} masks"
+            masks_to_buy += 1
 
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
     return f"Friends can go to {cafe.name}"
