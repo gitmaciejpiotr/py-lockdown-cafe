080fe712b917929f0749d3321b126810901209bc
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-02-02 12:22:34 +0100

    fixed parts not aligning with project requirements

--- app/cafe.py
+++ app/cafe.py
@@ -1,7 +1,10 @@
 import datetime
-from app.errors import (NotVaccinatedError,
-                        OutdatedVaccineError,
-                        NotWearingMaskError)
+
+from app.errors import (
+    NotVaccinatedError,
+    OutdatedVaccineError,
+    NotWearingMaskError
+)
 
 
 class Cafe:
@@ -10,10 +13,10 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str | None:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError
+            raise NotVaccinatedError("Visitor is not vaccinated!")
         elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError
+            raise OutdatedVaccineError("Visitors vaccine is outdated!")
         elif visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError
+            raise NotWearingMaskError("Every visitor have to wear mask!")
         else:
             return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from app.errors import NotWearingMaskError, VaccineError
 from app.cafe import Cafe
 
 
-def go_to_cafe(friends: list, cafe: Cafe) -> str | None:
+def go_to_cafe(friends: list[dict], cafe: Cafe) -> str | None:
     masks_to_buy = 0
     not_vaccinated = 0
 
@@ -18,5 +18,4 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str | None:
         return "All friends should be vaccinated"
     elif masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
-    else:
-        return f"Friends can go to {cafe.name}"
+    return f"Friends can go to {cafe.name}"
